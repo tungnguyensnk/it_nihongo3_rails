636d8c532842936de5546575924cbc806f134001
tungnguyensnk <tung.nd194714@sis.hust.edu.vn>
2023-01-10 15:51:50 +0700

    Add Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
   devise_for :users
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
