abf947efa60231063a0466a97752ca7196aedcfc
tungnguyensnk <tung.nd194714@sis.hust.edu.vn>
2023-01-30 09:30:32 +0700

    Add avatar on users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys: [:name, :age, :avatar]
+    devise_parameter_sanitizer.permit :account_update, keys: [:name, :age, :avatar]
+  end
 end
